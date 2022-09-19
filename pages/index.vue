2a315ed8a062aaf7cb95fab2071a8a83236d7d12
nntan2002 <nntan2002@gmail.com>
2022-09-19 17:41:07 +0700

    test

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="main-wheel">
+      <ul class="wheel"></ul>
+    </div>
+    <div>
+      <button class="btn-start">
+        Quay thưởng
+      </button>
+    </div>
+    <h1 class="msg"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
